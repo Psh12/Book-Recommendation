ca6b0654f5fa4a3bbb241ddabd5d77653bf7df5e
Ariel Torres <atorres3198@gmail.com>
2021-06-22 15:09:33 -0400

    Fixed App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Search from './components/Search';
+import Toggle from './components/Toggle';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/search' exact component={Search}/>
+          <Route path='/toggle' exact component={Toggle}/>
+        </Switch>
+      </Router>
+    </>
   );
 }
 
